4a739b9fede87c9af818a686ad2a952742916b9b
Truongk5 <161328778+ngvantruong05@users.noreply.github.com>
2024-03-25 21:25:45 +0700

    Create CommonFunction.h

---
+++ CommonFunction.h
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <SDL.h>
+#include <SDL_image.h>
+#include "CommonFunction.h"
+
+#define WIDTH_MAIN_OBJECT 80
+#define HEIGHT_MAIN_OBJECT 46
+using namespace std;
+
+int main(int argc, char* args[]) {
+    if (!COMMONFUNCTION::init()) {
+        std::cout << "Failed to initialize!" << std::endl;
+        return -1;
+    }
+    bool quit = false;
+    while (!quit) {
+        handleEvents(quit);
+        update();
+        render();
+        SDL_Delay(10);
+    }
+    COMMONFUNCTION::close();
+    return 0;
+}
